5e1ee0b7652d3380532dd83124471ff133f6d372
Stephen C Harris <stephen.harris@fda.hhs.gov>
2013-09-16 15:40:17 -0500

    Added pow() function.

--- common.rs
+++ common.rs
@@ -6,3 +6,16 @@ pub struct Deg(u8);
 
 #[deriving(Eq, IterBytes, TotalOrd, TotalEq, Clone, Ord)]
 pub struct Dim(uint);
+
+// utility functions
+
+#[inline(always)]
+pub fn pow(base: R, exp: uint) -> R {
+  let mut prod = 1 as R;
+  let mut i = exp;
+  while i != 0u {
+    prod *= base;
+    i -= 1;
+  } 
+  prod
+}
