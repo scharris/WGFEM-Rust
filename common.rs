d14b53dd2d21bb40038fb6e60ee6650db742cda6
Stephen C Harris <stephen.harris@fda.hhs.gov>
2013-09-21 12:23:37 -0500

    Replaced pow impl with possibly more efficient version.

--- common.rs
+++ common.rs
@@ -14,22 +14,19 @@ pub struct Dim(uint);
 pub static DEFAULT_INTEGRATION_REL_ERR: R = 1e-12;
 pub static DEFAULT_INTEGRATION_ABS_ERR: R = 1e-12;
 
-// utility functions
-
-#[inline(always)]
-pub fn pow(base: R, exp: uint) -> R {
+#[inline]
+pub fn pow(radix: R, exp: uint) -> R {
+  if exp == 0u { return 1 as R; }
+  if radix == 0. { return 0 as R; }
   let mut prod = 1 as R;
-  let mut i = exp;
-  while i != 0u {
-    prod *= base;
-    i -= 1;
-  } 
-  prod
-}
-
-#[test]
-fn test_pow() {
-  assert_eq!(pow(2f64, 0), 1f64);
-  assert_eq!(pow(2f64, 1), 2f64);
-  assert_eq!(pow(2f64, 2), 4f64);
+  let mut _exp = exp;
+  let mut multiplier = radix;
+  while (_exp > 0u) {
+    if _exp % 2u == 1u {
+      prod *= multiplier;
+    }
+    _exp /= 2u;
+    multiplier *= multiplier;
+  }
+ prod 
 }
