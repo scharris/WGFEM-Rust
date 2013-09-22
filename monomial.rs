918e3a08a1b56ff5b291bc5be217b6c3f11c9a26
Stephen C Harris <stephen.harris@fda.hhs.gov>
2013-09-22 12:12:31 -0500

    Add functions counting and generating monomials not exceeding a given degree.

--- monomial.rs
+++ monomial.rs
@@ -1,5 +1,7 @@
 use common::*;
 use std::option::*;
+use std::vec;
+use std::iter::{range_inclusive, AdditiveIterator};
 
 pub trait Monomial: Eq +
                     TotalEq +
@@ -10,7 +12,7 @@ pub trait Monomial: Eq +
                     ToStr +
                     Mul<Self,Self> {
 
-  fn domain_dim(witness: Option<Self>) -> uint;
+  fn domain_dim(_: Option<Self>) -> uint;
   
   fn value_at(&self, x: &[R]) -> R;
   
@@ -21,6 +23,8 @@ pub trait Monomial: Eq +
   fn map_exp(&self, coord: Dim, f: &fn(Deg) -> Deg) -> Self;
 
   fn one() -> Self;
+
+  fn mons_of_deg_le(deg: Deg) -> ~[Self];
 }
 
 
@@ -46,11 +50,10 @@ static one_3d: Mon3d = Mon3d { exps: [Deg(0),..3] };
 static one_4d: Mon4d = Mon4d { exps: [Deg(0),..4] };
 
 
-
 impl Monomial for Mon1d {
 
   #[inline(always)]
-  fn domain_dim(witness: Option<Mon1d>) -> uint {
+  fn domain_dim(_: Option<Mon1d>) -> uint {
     1u
   }
 
@@ -80,12 +83,16 @@ impl Monomial for Mon1d {
   fn one() -> Mon1d {
     one_1d
   }
+
+  fn mons_of_deg_le(deg: Deg) -> ~[Mon1d] {
+    vec::from_fn(*deg as uint + 1, |e| Mon1d { exps: [ Deg(e as u8) ] }) 
+  }
 }
 
 impl Monomial for Mon2d {
 
   #[inline(always)]
-  fn domain_dim(witness: Option<Mon2d>) -> uint {
+  fn domain_dim(_: Option<Mon2d>) -> uint {
     2u
   }
   
@@ -118,12 +125,23 @@ impl Monomial for Mon2d {
   fn one() -> Mon2d {
     one_2d
   }
+
+  fn mons_of_deg_le(deg: Deg) -> ~[Mon2d] {
+    let mut mons: ~[Mon2d] = vec::with_capacity(num_mons_of_deg_le(deg, Dim(2)));
+    for e0 in range_inclusive(0, *deg) {
+      for e1 in range_inclusive(0, *deg - e0) {
+        mons.push(Mon2d { exps: [Deg(e0), Deg(e1)] });
+      }
+    }
+    mons
+  }
+
 }
 
 impl Monomial for Mon3d {
 
   #[inline(always)]
-  fn domain_dim(witness: Option<Mon3d>) -> uint {
+  fn domain_dim(_: Option<Mon3d>) -> uint {
     3u
   }
   
@@ -159,12 +177,24 @@ impl Monomial for Mon3d {
   fn one() -> Mon3d {
     one_3d
   }
+  
+  fn mons_of_deg_le(deg: Deg) -> ~[Mon3d] {
+    let mut mons: ~[Mon3d] = vec::with_capacity(num_mons_of_deg_le(deg, Dim(3)));
+    for e0 in range_inclusive(0, *deg) {
+      for e1 in range_inclusive(0, *deg - e0) {
+        for e2 in range_inclusive(0, *deg - e0 - e1) {
+          mons.push(Mon3d { exps: [Deg(e0), Deg(e1), Deg(e2)] });
+        }
+      }
+    }
+    mons
+  }
 }
 
 impl Monomial for Mon4d {
 
   #[inline(always)]
-  fn domain_dim(witness: Option<Mon4d>) -> uint {
+  fn domain_dim(_: Option<Mon4d>) -> uint {
     4u
   }
 
@@ -202,6 +232,20 @@ impl Monomial for Mon4d {
   #[inline(always)]
   fn one() -> Mon4d {
     one_4d
+  }
+  
+  fn mons_of_deg_le(deg: Deg) -> ~[Mon4d] {
+    let mut mons: ~[Mon4d] = vec::with_capacity(num_mons_of_deg_le(deg, Dim(4)));
+    for e0 in range_inclusive(0, *deg) {
+      for e1 in range_inclusive(0, *deg - e0) {
+        for e2 in range_inclusive(0, *deg - e0 - e1) {
+          for e3 in range_inclusive(0, *deg - e0 - e1 - e2) {
+            mons.push(Mon4d { exps: [Deg(e0), Deg(e1), Deg(e2), Deg(e3)] });
+          }
+        }
+      }
+    }
+    mons
   }
 }
 
@@ -253,25 +297,25 @@ eq_impl!(Mon4d)
 
 // Implement IterBytes for hashing.
 impl IterBytes for Mon1d {
-  fn iter_bytes(&self, lsb0: bool, f: &fn(buf: &[u8]) -> bool) -> bool {
+  fn iter_bytes(&self, _: bool, f: &fn(buf: &[u8]) -> bool) -> bool {
     f(&[*self.exps[0]])
   }
 }
 
 impl IterBytes for Mon2d {
-  fn iter_bytes(&self, lsb0: bool, f: &fn(buf: &[u8]) -> bool) -> bool {
+  fn iter_bytes(&self, _: bool, f: &fn(buf: &[u8]) -> bool) -> bool {
     f(&[*self.exps[0], *self.exps[1]])
   }
 }
 
 impl IterBytes for Mon3d {
-  fn iter_bytes(&self, lsb0: bool, f: &fn(buf: &[u8]) -> bool) -> bool {
+  fn iter_bytes(&self, _: bool, f: &fn(buf: &[u8]) -> bool) -> bool {
     f(&[*self.exps[0], *self.exps[1], *self.exps[2]])
   }
 }
 
 impl IterBytes for Mon4d {
-  fn iter_bytes(&self, lsb0: bool, f: &fn(buf: &[u8]) -> bool) -> bool {
+  fn iter_bytes(&self, _: bool, f: &fn(buf: &[u8]) -> bool) -> bool {
     f(&[*self.exps[0], *self.exps[1], *self.exps[2], *self.exps[3]])
   }
 }
@@ -381,3 +425,47 @@ impl Mul<Mon4d, Mon4d> for Mon4d {
   }
 }
 
+
+// auxiliary functions
+
+
+fn num_mons_of_deg_eq(deg: Deg, dom_dim: Dim) -> uint {
+  multiset_choose(*dom_dim as uint, *deg as uint)
+}
+
+pub fn num_mons_of_deg_le(max_deg: Deg, dom_dim: Dim) -> uint {
+  range_inclusive(0, *max_deg)
+    .map(|deg| num_mons_of_deg_eq(Deg(deg), dom_dim))
+    .sum()
+}
+
+#[inline]
+fn multiset_choose(n: uint, k: uint) -> uint {
+  binomial(n + k - 1, k)
+}
+
+fn binomial(n: uint, k: uint) -> uint {
+  if k == 0u || n == k { 1u }
+  else if k > n { 0u }
+  else {
+    let k = if k > n - k { n - k } else { k };
+    if k == 1 { n }
+    else {
+      let mut b = 1;
+      for i in range_inclusive(1,k) {
+        b *= (n - (k - i));
+        b /= i;
+      }
+      b
+    }
+  }
+}
+
+
+#[test]
+fn test_binomial() {
+  assert_eq!(binomial(5, 0), 1);
+  assert_eq!(binomial(5, 5), 1); 
+  assert_eq!(binomial(5, 1), 5);
+  assert_eq!(binomial(5, 2), 10);
+}
