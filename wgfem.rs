41c099cdc049f88eb4af0bbd90a027950de0314f
Stephen C Harris <stephen.harris@fda.hhs.gov>
2013-10-10 15:50:55 -0500

    Moved tests into submodule.

--- wgfem.rs
+++ wgfem.rs
@@ -10,37 +10,26 @@
 extern mod extra;
 
 pub mod common;
-
 pub mod monomial;
-#[cfg(test)]
-mod test_monomial;
-
 pub mod polynomial;
-#[cfg(test)]
-mod test_polynomial;
-
 pub mod vector_monomial;
-#[cfg(test)]
-mod test_vector_monomial;
-
 pub mod mesh;
-
 pub mod rectangle_mesh;
-#[cfg(test)]
-mod test_rectangle_mesh;
-
 pub mod dense_matrix;
-#[cfg(test)]
-mod test_dense_matrix;
-
 pub mod weak_gradient;
-#[cfg(test)]
-mod test_weak_gradient;
-
 pub mod wg_basis;
-#[cfg(test)]
-mod test_wg_basis;
-
 pub mod lapack;
 mod quadrature;
+
+#[cfg(test)]
+mod tests {
+  mod test_monomial;
+  mod test_polynomial;
+  mod test_vector_monomial;
+  // no tests for abstract mesh
+  mod test_rectangle_mesh;
+  mod test_dense_matrix;
+  mod test_weak_gradient;
+  mod test_wg_basis;
+}
 
