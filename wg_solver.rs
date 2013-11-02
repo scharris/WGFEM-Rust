323cd8ba384887ab17de4859f7b391131516febc
Stephen C Harris <stephen.harris@fda.hhs.gov>
2013-11-02 15:06:42 -0500

    Add boundary projections to solution structure.

--- wg_solver.rs
+++ wg_solver.rs
@@ -6,9 +6,10 @@ use mesh::{Mesh, FENum, OShape, SideFace};
 use wg_basis::{WgBasis, FaceMonNum, BasisElNum};
 use projection::Projector;
 use variational_bilinear_form::VariationalBilinearForm;
+use lapack;
 
 use std::hashmap::HashMap;
-use vbf_laplace::VBFLaplace;
+use vbf_laplace::VBFLaplace; // TODO: remove when Rust ICE is fixed
 
 
 /* METHOD
@@ -31,11 +32,10 @@ use vbf_laplace::VBFLaplace;
  * the *transpose* of the basis-vs-basis vbf values matrix.
  */
 
-
-
 struct WgSolution<'self,Mon,MeshT> {
   basis_coefs: ~[R],
-  basis: &'self WgBasis<Mon,MeshT>
+  basis: &'self WgBasis<Mon,MeshT>,
+  bnd_projs: BoundaryProjections<'self,Mon>
 }
 
 /*
@@ -43,28 +43,27 @@ pub fn solve<'self, Mon: Monomial, MeshT: Mesh<Mon>, VBF: VariationalBilinearFor
        (vbf: &'self VBF, f: &fn(&[R])->R, g: &fn(&[R])->R) -> WgSolution<'self,Mon,MeshT> {
 */
 fn solve_laplace<'a, Mon:Monomial, MeshT:Mesh<Mon>>
-       (basis: &'a WgBasis<Mon,MeshT>, f: &fn(&[R])->R, g: &fn(&[R])->R) -> WgSolution<'a,Mon,MeshT> {
+   (basis: &'a WgBasis<Mon,MeshT>, f: &fn(&[R])->R, g: &fn(&[R])->R) -> WgSolution<'a,Mon,MeshT> {
   
   // TODO: Replace with vbf trait parameter when Rust internal compiler error is fixed. https://github.com/mozilla/rust/issues/10201
   let vbf = &VBFLaplace::new(None, basis);
 
-  let boundary_projs = &boundary_projections(g, basis);
+  let bnd_projs = boundary_projections(g, basis);
 
-  let sys_matrix = vbf.basis_els_vs_basis_els_transpose();
+  let sys_m = vbf.basis_els_vs_basis_els_transpose();
 
   let sys_rhs = DenseMatrix::from_fn(basis.num_els(), 1, |i,_| 
     ip_on_ints(|x|f(x), BasisElNum(i), basis)
     - 
-    vbf_boundary_projs_vs_bel(vbf, boundary_projs, BasisElNum(i), basis)
+    vbf_bnd_projs_vs_bel(vbf, &bnd_projs, BasisElNum(i), basis)
   );
-  
-/* TODO: Do sparse solve here.
-  const sol_basis_coefs = m \ sys_rhs(f, g_projs, wg_solver.vbf, wg_solver.basis)
-  WGSolution(vec(sol_basis_coefs), g_projs, wg_solver.basis)
-*/
+
+  let sol = lapack::solve_sparse_structurally_symmetric(&sys_m, &sys_rhs, vbf.is_symmetric());
+
   WgSolution {
-    basis_coefs: ~[], // TODO
+    basis_coefs: sol,
     basis: basis,
+    bnd_projs: bnd_projs,
   }
 }
 
@@ -83,9 +82,9 @@ fn ip_on_ints<Mon:Monomial, MeshT: Mesh<Mon>>
 // onto outside boundary segments vs. the given basis element. This is the vbf(Q_b g, b_i) term
 // of the right hand side of (sys). The implementation uses the Element Summability and Locality
 // properties of supported variational forms (see VBF module for a discussion).
-fn vbf_boundary_projs_vs_bel<'a, Mon:Monomial, MeshT: Mesh<Mon>>
+fn vbf_bnd_projs_vs_bel<'a, Mon:Monomial, MeshT: Mesh<Mon>>
    (vbf: &VBFLaplace<'a,Mon,MeshT>,
-    boundary_projs: &BoundaryProjections<'a,Mon>,
+    bnd_projs: &BoundaryProjections<'a,Mon>,
     bel: BasisElNum,
     basis: &WgBasis<Mon,MeshT>) -> R {
 
@@ -98,7 +97,7 @@ fn vbf_boundary_projs_vs_bel<'a, Mon:Monomial, MeshT: Mesh<Mon>>
     let bel_monn = basis.int_rel_mon_num(bel);
     // Sum of vbf(p,bel) for all boundary side projections p on the boundary sides of bel's fe.
     range(0, mesh.num_side_faces_for_shape(fe_oshape)).fold(0 as R, |sum, sf| {
-        match boundary_projs.find(&(fe, SideFace(sf))) {
+        match bnd_projs.find(&(fe, SideFace(sf))) {
           Some(bnd_proj) => { 
             bnd_proj.foldl_numbered_terms(sum, |sum, (proj_monn, proj_coef, _)| {
               sum + proj_coef * vbf.side_mon_vs_int_mon(fe_oshape, 
@@ -121,7 +120,7 @@ fn vbf_boundary_projs_vs_bel<'a, Mon:Monomial, MeshT: Mesh<Mon>>
     fe_incls_of_bel_supp.iter().fold(0 as R, |sum, &(fe, bel_sf)| {
       let fe_oshape = mesh.oriented_shape_for_fe(fe);
       range(0, mesh.num_side_faces_for_shape(fe_oshape)).fold(sum, |sum, sf| {
-        match boundary_projs.find(&(fe, SideFace(sf))) {
+        match bnd_projs.find(&(fe, SideFace(sf))) {
           Some(bnd_proj) =>
             bnd_proj.foldl_numbered_terms(sum, |sum, (proj_monn, proj_coef, _)| {
               sum + proj_coef * vbf.side_mon_vs_side_mon_fe_contr(fe_oshape, 
