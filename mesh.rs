56d08f4568c9cf7619ce66c1ec6b20ee34346967
Steve Harris <steveofar@gmail.com>
2013-12-26 17:23:11 -0600

    Formatting and abbreviations changes.

--- mesh.rs
+++ mesh.rs
@@ -54,28 +54,28 @@ pub trait Mesh<Mon> {
 
   fn oriented_shape_for_fe(&self, fe: FENum) -> OShape;
 
-  fn num_side_faces_for_oshape(&self, oshape: OShape) -> uint;
+  fn num_side_faces_for_oshape(&self, os: OShape) -> uint;
 
-  // The dependent dim is, for a given side, the dimension j which is affine-
-  // dependent on the other dimensions on the side. That is, the function
-  // returns a j for which c_0,...,c_d exist such that
+  // The dependent dim is, for a given side, a chosen dimension j which is affine-dependent on
+  // the other dimensions on the side. That is, the function returns a j for which c_0,...,c_d
+  // exist such that
   //   x_j = c_0 + sum_{i=1..d} c_i x_i for all (x_1,...,x_d) in the side.
-  // There may be more than one such coordinate number, in which case any 
-  // one of these may be returned depending on the mesh implementation.
-  fn dependent_dim_for_oshape_side(&self, oshape: OShape, side_face: SideFace) -> Dim;
+  // There may be more than one such coordinate number, in which case any one of these may be
+  // returned depending on the mesh implementation.
+  fn dependent_dim_for_oshape_side(&self, os: OShape, sf: SideFace) -> Dim;
 
   fn fe_inclusions_of_nb_side(&self, side_num: NBSideNum) -> NBSideInclusions;
 
   // Return non-boundary side number of the indicated fe relative side.
-  fn nb_side_num_for_fe_side(&self, fe: FENum, side_face: SideFace) -> NBSideNum;
+  fn nb_side_num_for_fe_side(&self, fe: FENum, sf: SideFace) -> NBSideNum;
 
-  fn is_boundary_side(&self, fe: FENum, side_face: SideFace) -> bool;
+  fn is_boundary_side(&self, fe: FENum, sf: SideFace) -> bool;
 
   fn num_boundary_sides(&self) -> uint;
   
   fn boundary_fes_by_oshape_side(&self) -> ~[~[~[FENum]]]; // oshape, side face -> fes
   
-  fn shape_diameter_inv(&self, oshape: OShape) -> R;
+  fn shape_diameter_inv(&self, os: OShape) -> R;
 
   fn max_fe_diameter(&self) -> R;
 
@@ -83,63 +83,29 @@ pub trait Mesh<Mon> {
 
   fn max_num_shape_sides(&self) -> uint;
 
-
   // integration functions
   
-  fn intg_global_fn_on_fe_int(&self,
-       g: |&[R]| -> R,
-       fe: FENum) -> R;
+  fn intg_global_fn_on_fe_int(&self, f: |&[R]| -> R, fe: FENum) -> R;
 
-  fn intg_global_fn_x_facerel_mon_on_fe_int(&self,
-       g: |&[R]| -> R,
-       mon: Mon,
-       fe: FENum) -> R;
+  fn intg_global_fn_x_facerel_mon_on_fe_int(&self, f: |&[R]| -> R, mon: Mon, fe: FENum) -> R;
 
-  fn intg_global_fn_x_facerel_mon_on_fe_side(&self,
-       g: |&[R]| -> R,
-       mon: Mon,
-       fe: FENum,
-       side_face: SideFace) -> R;
+  fn intg_global_fn_x_facerel_mon_on_fe_side(&self, f: |&[R]| -> R, mon: Mon, fe: FENum, sf: SideFace) -> R;
   
-  fn intg_mixed_global_and_facerel_fn_on_fe_int(&self,
-       f: |&[R], &[R]| -> R,
-       fe: FENum) -> R; 
+  fn intg_mixed_global_and_facerel_fn_on_fe_int(&self, f: |&[R], &[R]| -> R, fe: FENum) -> R; 
 
-  fn intg_facerel_poly_on_oshape_int<P:Polynomial<Mon>>(&self,
-       p: &P,
-       oshape: OShape) -> R;
+  fn intg_facerel_poly_on_oshape_int<P:Polynomial<Mon>>(&self, p: &P, os: OShape) -> R;
 
-  fn intg_facerel_poly_x_facerel_poly_on_oshape_side<P:Polynomial<Mon>>(&self,
-       p1: &P,
-       p2: &P,
-       oshape: OShape,
-       side_face: SideFace) -> R;
+  fn intg_facerel_poly_x_facerel_poly_on_oshape_side<P:Polynomial<Mon>>(&self, p1: &P, p2: &P, os: OShape, sf: SideFace) -> R;
 
-  fn intg_facerel_mon_on_oshape_int(&self,
-       mon: Mon,
-       oshape: OShape) -> R;
+  fn intg_facerel_mon_on_oshape_int(&self, mon: Mon, os: OShape) -> R;
 
-  fn intg_facerel_mon_on_oshape_side(&self,
-       mon: Mon,
-       oshape: OShape,
-       side_face: SideFace) -> R;
+  fn intg_facerel_mon_on_oshape_side(&self, mon: Mon, os: OShape, sf: SideFace) -> R;
 
-  fn intg_facerel_mon_x_facerel_poly_on_oshape_side<P:Polynomial<Mon>>(&self,
-       mon: Mon,
-       p: &P,
-       oshape: OShape,
-       side_face: SideFace) -> R;
+  fn intg_facerel_mon_x_facerel_poly_on_oshape_side<P:Polynomial<Mon>>(&self, mon: Mon, p: &P, os: OShape, sf: SideFace) -> R;
 
-  fn intg_intrel_mon_x_siderel_mon_on_oshape_side(&self,
-       int_mon: Mon,
-       side_mon: Mon, oshape: OShape,
-       side_face: SideFace) -> R;
+  fn intg_intrel_mon_x_siderel_mon_on_oshape_side(&self, int_mon: Mon, side_mon: Mon, os: OShape, sf: SideFace) -> R;
   
-  fn intg_siderel_mon_x_intrel_vmon_dot_normal_on_oshape_side(&self,
-       mon: Mon,
-       q: &VectorMonomial<Mon>,
-       oshape: OShape,
-       side_face: SideFace) -> R;
+  fn intg_siderel_mon_x_intrel_vmon_dot_normal_on_oshape_side(&self, mon: Mon, q: &VectorMonomial<Mon>, os: OShape, sf: SideFace) -> R;
  
 }
 
