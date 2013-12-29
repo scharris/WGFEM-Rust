d1d9a59ca96956b4ebf2978f875086e6a3df3e00
Steve Harris <steveofar@gmail.com>
2013-12-28 19:09:19 -0600

    Use composerxe symlink for cross platform compile.

--- cc.sh
+++ cc.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-MKLROOT=${MKLROOT:-"/opt/intel/composer_xe_2013_sp1.0.080/mkl"}
-echo Using MKLROOT "$MKLROOT".
-
 PROJHOME=`dirname $BASH_SOURCE[0]`
+MKLROOT=${MKLROOT:-"/opt/intel/composerxe/mkl"}
+MKL_INTEL_LP64="${MKLROOT}/lib/libmkl_intel_lp64.a" 
+MKL_INTEL_THREAD="${MKLROOT}/lib/libmkl_intel_thread.a"
+MKL_CORE="${MKLROOT}/lib/libmkl_core.a"
+MKL_IOMP5="${MKLROOT}/../compiler/lib/libiomp5.a"
+echo Using MKLROOT "$MKLROOT".
 
 if [ ! -d "$PROJHOME/lib/mkl/" ]; then
   echo "Copying MKL library files into project."
   mkdir -p "$PROJHOME/lib/mkl/";
-  cp "${MKLROOT}/lib/intel64/libmkl_intel_lp64.a" \
-     "${MKLROOT}/lib/intel64/libmkl_intel_thread.a" \
-     "${MKLROOT}/lib/intel64/libmkl_core.a" \
-     "${MKLROOT}/../compiler/lib/intel64/libiomp5.a" \
-     "$PROJHOME/lib/mkl/"
+  cp "$MKL_INTEL_LP64" "$MKL_INTEL_THREAD" "$MKL_CORE" "$MKL_IOMP5" "$PROJHOME/lib/mkl/"
 else
   echo "MKL libraries already present in project."
 fi
 
 echo "Compiling linear algebra wrapper functions."
-gcc -m64 -w -I"${MKLROOT}/include" -o lib/linear_algebra.o -c linear_algebra.c 
+gcc -m64 -w -I"${MKLROOT}/include" -c linear_algebra.c -o lib/linear_algebra.o 
 
